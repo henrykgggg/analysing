1f9ae5476e64e08580d7fc25061acc5369b1774b
henry <88551100+henrykgggg@users.noreply.github.com>
2023-05-25 12:22:27 +0200

    added expense dynamically

--- my-app/src/components/ExpensItem.js
+++ my-app/src/components/ExpensItem.js
@@ -1,14 +1,21 @@
 import './ExpenseItem.css'
 
 const ExpenseItem = () => {
+const expenseDate =  new Date(2021 , 2 , 24)
+const expenseTitle = 'car insurance';
+const expenseAmount = 295.67;
+
+
+
   return (
-    <div>
-      <div>march 24th 2021</div>
-      <div>
-        <h2>Car insurance</h2> 
+    <div className='expense-item'>
+      <div>{expenseDate}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>  
       </div>
-      <div>$ 23.8</div>
+      <div className='expense-item__price'>${expenseAmount}</div>
     </div>
   );
 };
 export default ExpenseItem;
+  
