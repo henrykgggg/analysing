27b1f1ef4bc3a2fb3f0fa7b5bff94dffbeaa41ae
henry <88551100+henrykgggg@users.noreply.github.com>
2023-05-30 18:52:40 +0200

    created date in separate component

--- my-app/src/components/ExpensItem.js
+++ my-app/src/components/ExpensItem.js
@@ -1,16 +1,11 @@
 import './ExpenseItem.css'
-
+import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
-const month = props.date.toLocaleString('en-US', {month:'long'});
-const day = props.date.toLocaleString('en-US', {day:'2-digit'});
-const year = props.date.getFullYear();
 
   return (
     <div className='expense-item'>
       <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
+<ExpenseDate date={props.date}/>
       </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
