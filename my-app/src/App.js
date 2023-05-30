b2af2556af1a4e7c66e1a7e518424da7781c60c6
henry <88551100+henrykgggg@users.noreply.github.com>
2023-05-30 15:34:56 +0200

    used array for expenses

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,59 @@
-
 import ExpenseItem from "./components/ExpensItem";
-function App() {
+const App = () => {
   const expenses = [
     {
-    title:'car insurance', amount:456, date: new Date(2022,3,1)
-  },
-  {
-    title:'car insurance', amount:456, date: new Date(2022,3,1)
-  },
-  {
-    title:'car insurance', amount:456, date: new Date(2022,3,1)
-  },
-  {
-    title:'car insurance', amount:456, date: new Date(2022,3,1)
-  }
-]
+      title: "car insurance",
+      amount: 456,
+      date: new Date(2022, 3, 1),
+    },
+    {
+      id: "1",
+      title: "car insurance",
+      amount: 466,
+      date: new Date(2022, 3, 1),
+    },
+    {
+      id: "2",
+      title: "ambulance",
+      amount: 456,
+      date: new Date(2019, 3, 9),
+    },
+    {
+      id: "3",
+      title: "hericopteer",
+      amount: 458,
+      date: new Date(2023, 3, 1),
+    },
+    {
+      id: "4",
+      title: "bus",
+      amount: 486,
+      date: new Date(2022, 8, 1),
+    },
+  ];
   return (
     <div>
-<ExpenseItem />
-<ExpenseItem />
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- my-app/src/components/ExpensItem.js
+++ my-app/src/components/ExpensItem.js
@@ -1,21 +1,18 @@
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
-const expenseDate =  new Date(2021 , 2 , 24) 
-const expenseTitle = 'car insurance';
-const expenseAmount = 295.67;
+const ExpenseItem = (props) => {
 
 
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>  
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <div className='expense-item__price'>${expenseAmount}</div>
     </div>
   );
-};
+}
 export default ExpenseItem;
   
