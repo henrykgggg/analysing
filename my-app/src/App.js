15378cbde9461dcf497944513dc57c6c10e7cd10
henry <88551100+henrykgggg@users.noreply.github.com>
2023-05-30 14:58:26 +0200

    added list of expenses

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,23 @@
 
 import ExpenseItem from "./components/ExpensItem";
 function App() {
+  const expenses = [
+    {
+    title:'car insurance', amount:456, date: new Date(2022,3,1)
+  },
+  {
+    title:'car insurance', amount:456, date: new Date(2022,3,1)
+  },
+  {
+    title:'car insurance', amount:456, date: new Date(2022,3,1)
+  },
+  {
+    title:'car insurance', amount:456, date: new Date(2022,3,1)
+  }
+]
   return (
     <div>
+<ExpenseItem />
 <ExpenseItem />
     </div>
   );
--- my-app/src/components/ExpensItem.js
+++ my-app/src/components/ExpensItem.js
@@ -1,7 +1,7 @@
 import './ExpenseItem.css'
 
 const ExpenseItem = () => {
-const expenseDate =  new Date(2021 , 2 , 24)
+const expenseDate =  new Date(2021 , 2 , 24) 
 const expenseTitle = 'car insurance';
 const expenseAmount = 295.67;
 
@@ -9,7 +9,7 @@ const expenseAmount = 295.67;
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate}</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
         <h2>{expenseTitle}</h2>  
       </div>
