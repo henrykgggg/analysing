240fbb40c342e384b2ce228a15a40745495b58e2
henry <88551100+henrykgggg@users.noreply.github.com>
2023-06-01 13:48:43 +0200

    addded event listner on button

--- my-app/src/components/ExpensItem.js
+++ my-app/src/components/ExpensItem.js
@@ -1,16 +1,25 @@
+import React, { useState } from 'react'
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 const ExpenseItem = (props) => {
+  const [title , setTitle] = useState(props.title)
+const onClickHandler = () => {
+  setTitle('updated')
+}
+
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <div>
         <ExpenseDate date={props.date} />
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>                                                                        
-        <div className="expense-item__price">${props.amount}</div>                                                                                            
+        <h2>{title}</h2>                                                                        
+        <div className="expense-item__price">${props.amount}</div>  
+        <button onClick={onClickHandler}>click me</button>                                                                                          
       </div>                                               
-    </div>
+    </Card>
   );
 };
 export default ExpenseItem;
