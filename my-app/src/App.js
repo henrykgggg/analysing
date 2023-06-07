0e35f5f7b1341f59263a0f7d2b301a4172a000f7
henry <88551100+henrykgggg@users.noreply.github.com>
2023-06-07 06:57:39 +0200

    rendering from parent to child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,45 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
-const App = () => {
-  const expenses = [
-    {
-      id: "02",
-      title: "car insurance",
-      amount: 456,
-      date: new Date(2022, 3, 1),
-    },
-    {
-      id: "1",
-      title: "car insurance",
-      amount: 466,
-      date: new Date(2022, 3, 1),
-    },
-    {
-      id: "2",
-      title: "ambulance",
-      amount: 456,
-      date: new Date(2019, 3, 9),
-    },
-    {
-      id: "3",
-      title: "hericopteer",
-      amount: 458,
-      date: new Date(2023, 3, 1),
-    },
-    {
-      id: "4",
-      title: "bus",
-      amount: 486,
-      date: new Date(2022, 8, 1),
-    },
-  ];
+const DUMMYExpenses = [
+  {
+    id: "02",
+    title: "car insurance",
+    amount: 456,
+    date: new Date(2022, 3, 1),
+  },
+  {
+    id: "1",
+    title: "car insurance",
+    amount: 466,
+    date: new Date(2022, 3, 1),
+  },
+  {
+    id: "2",
+    title: "ambulance",
+    amount: 456,
+    date: new Date(2019, 3, 9),
+  },
+  {
+    id: "3",
+    title: "hericopteer",
+    amount: 458,
+    date: new Date(2023, 3, 1),
+  },
+  {
+    id: "4",
+    title: "bus",
+    amount: 486,
+    date: new Date(2022, 8, 1),
+  },
+];
 
+const App = () => {
+const[expenses , setExpenses] =useState(DUMMYExpenses)
 const NewExpenseHandler = (expensesLast) =>{
-console.log(expensesLast)
+setExpenses((prevExpenses)=>{
+  return [expensesLast,...prevExpenses]
+})
 }
 
 
